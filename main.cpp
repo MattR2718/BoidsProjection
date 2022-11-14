fabec929bfd12e32fa1ddd10bbeae766164224e8
MattR2718 <66754574+MattR2718@users.noreply.github.com>
2022-11-14 12:13:32 +0000

    Basic Window

--- main.cpp
+++ main.cpp
@@ -1,5 +1,37 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, world!\n";
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    //Create a window that the program will draw to
+    sf::RenderWindow window(sf::VideoMode(600, 600), "Boids Projection");
+    //Limit the windows frame rate to 30
+    window.setFramerateLimit(30);
+
+    //Run program while window is open
+    while (window.isOpen())
+    {
+        //Loop over all events since last loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            //If close requested then close window
+            if (event.type == sf::Event::Closed){
+                window.close();
+            }
+        }
+
+        //Clear window with black colour
+        window.clear(sf::Color::Black);
+
+        //Create a circle to draw to screen
+        sf::CircleShape shape(300);
+        //Set shape colour
+        shape.setFillColor(sf::Color::Green);
+        //Draw shape to screen
+        window.draw(shape);
+        //End the current frame
+        window.display();
+    }
 }
